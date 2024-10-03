254d838ddbef8a7f03748f9d4f9ef8ea830f5cdb
gaurav <145903987+gaurav987364@users.noreply.github.com>
2024-10-03 13:16:10 +0530

    okokokok

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,20 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import mongoose, { Mongoose } from 'mongoose';
+import mongoose from 'mongoose';
 
-// let isConnected : boolean = false;
-interface MongooseConn {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
-}
-let cached: MongooseConn = (global as any).mongoose;
-
-if(!cached){
-    cached = (global as any).mongoose = {
-        conn: null,
-        promise: null,
-    }
-}
-
+let isConnected : boolean = false;
 export const connectToDatabse = async ()=>{
     mongoose.set('strictQuery', true); // set strict query for empty field se bachne k liye
     
@@ -22,30 +9,19 @@ export const connectToDatabse = async ()=>{
         return console.log('missing MONGODB_URL');
     }
 
-    // if(isConnected){
-    //     return console.log('Already connected to database');
-    // }
+    if(isConnected){
+        return console.log('Already connected to database');
+    }
 
-    // try {
-    //     await mongoose.connect(process.env.MONGODB_URL, {
-    //         dbName: "techflow",
-    //         bufferCommands:false,
-    //         connectTimeoutMS:30000,
-    //     })
-    //     isConnected = true;
-    //     console.log('Connected to database');
-    // } catch (error) {
-    //     console.error('Failed to connect to database', error);
-    // }
-
-    if(cached.conn) return cached.conn;
-
-    cached.promise = cached.promise || 
-    mongoose.connect(process.env.MONGODB_URL, {
-        dbName: 'techflow',
-        bufferCommands: false,
-        connectTimeoutMS: 30000
-    })
-    cached.conn = await cached.promise;
-    return cached.conn;
+    try {
+        await mongoose.connect(process.env.MONGODB_URL, {
+            dbName: "techflow",
+            bufferCommands:false,
+            connectTimeoutMS:30000,
+        })
+        isConnected = true;
+        console.log('Connected to database');
+    } catch (error) {
+        console.error('Failed to connect to database', error);
+    }
 }
